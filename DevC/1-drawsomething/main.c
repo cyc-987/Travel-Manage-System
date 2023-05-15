d0ba245d54a65012f0c4cbd1f40279bf23f85a31
cyc-987 <2516803593@qq.com>
2023-05-15 09:15:53 +0800

    增加main.c和全局头文件

---
+++ DevC/1-drawsomething/main.c
@@ -0,0 +1,14 @@
+#include "myHeader.h"
+
+void display()
+{
+}
+
+void Main()
+{
+	SetWindowTitle("旅游管理系统");
+	SetWindowSize(10,6);
+	InitGraphics();
+
+	display();
+}
---
+++ DevC/1-drawsomething/myHeader.h
@@ -0,0 +1,12 @@
+#ifndef _MY_HEADER_H_
+#define _MY_HEADER_H_
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+#include "graphics.h"
+#include "extgraph.h"
+#include "imgui.h"
+#endif
