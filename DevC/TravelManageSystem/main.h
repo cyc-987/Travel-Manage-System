c99ed831f4327d0b0b513b4899d386adcf55c337
cyc-987 <2516803593@qq.com>
2023-05-18 00:30:41 +0800

    头文件增加数据结构

--- DevC/TravelManageSystem/main.h
+++ DevC/TravelManageSystem/main.h
@@ -6,4 +6,29 @@
 extern int systemStatus;
 extern bool isLog;
 
+typedef struct Date
+{
+    int year;
+    int month;
+    int date;
+}Date;
+
+typedef struct travelItem{
+    int ID;
+
+    char name[100];
+    Date startDate;
+    Date endDate;
+
+    double price;
+    int numberReserved;
+    int numberTotal;
+    bool isReserved;
+    double rate;
+
+    char keyword[50];
+    char detail[200];
+}
+
+
 #endif
