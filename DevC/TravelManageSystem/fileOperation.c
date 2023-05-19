3cee435e424e26ae2bda7488d95fee1ced85d457
cyc-987 <2516803593@qq.com>
2023-05-19 23:49:03 +0800

    完成文件操作，增加调试输出，增加注释，修复bug

--- DevC/TravelManageSystem/fileOperation.c
+++ DevC/TravelManageSystem/fileOperation.c
@@ -17,12 +17,19 @@ int openFile()
     fp = fopen("data","rb");
     if(fp == NULL){
         FileHead = NULL;
+        Log("file failed to open");
         return 1;//file is not existed
-    } 
+    }
+    
+    Log("file opened successfully"); 
 
-    fread(FileHead,sizeof(fileHead),1,fp);
+    fileHead *FileHeadTemp = (fileHead*)malloc(sizeof(fileHead));
+    fread(FileHeadTemp,sizeof(fileHead),1,fp);
+    FileHead = FileHeadTemp;
     int itemNum;
     itemNum = FileHead->num;
+
+    Log("file head read successfully");
 
     travelItem *temp1,*temp2;
     int count;
@@ -30,6 +37,7 @@ int openFile()
     itemStructSize = sizeof(travelItem);
     itemHead = temp1 = temp2 = NULL;
     for(count = 1;count<=itemNum;count++){
+        temp1 = (travelItem*)malloc(itemStructSize);
         fread(temp1,itemStructSize,1,fp);
         if(count == 1){
             itemHead = temp1;
@@ -40,15 +48,21 @@ int openFile()
         if(count == itemNum) temp2->nextItem = NULL;
     }
 
+    Log("main data read completed");
+
     fclose(fp);
+    Log("file closed successfully");
     return 0;
 }
 
-int saveFile(travelItem *itemHead,int itemNum)
+int saveFile()
 {
     FILE *fp;
     fp = fopen("data","w+");
-    if(fp == NULL)return 1;//save file failed
+    if(fp == NULL){
+        Log("file failed to save");
+        return 1;
+        }//save file failed
 
     fwrite(FileHead,sizeof(fileHead),itemNum,fp);
     int count;
@@ -61,5 +75,82 @@ int saveFile(travelItem *itemHead,int itemNum)
     }
 
     fclose(fp);
+    return 0;
+}
+
+travelItem* createItem(int ID, Date startDate, Date endDate, double price, 
+                int numberReserved, int numberTotal, bool isReserved,
+                double rate, char* keyword, char* detail)
+{
+    travelItem *temp;
+    temp = (travelItem*)malloc(sizeof(travelItem));
+    temp->ID = ID;
+    temp->startDate = startDate;
+    temp->endDate = endDate;
+    temp->price = price;
+    temp->numberReserved = numberReserved;
+    temp->numberTotal = numberTotal;
+    temp->isReserved = isReserved;
+    temp->rate = rate;
+    strcpy(temp->keyword,keyword);
+    strcpy(temp->detail,detail);
+    temp->nextItem = NULL;
+
+    Log("created a new travelItem successfully");
+
+    return temp;
+}
+
+int addItem(travelItem *itemToAdd)
+{
+    travelItem *ptr;
+    ptr = NULL;
+    if(itemHead == NULL){
+        itemHead = itemToAdd;
+        itemHead->nextItem = NULL;
+    }else{
+        ptr = itemHead;
+        while(ptr->nextItem != NULL){
+            if(ptr->ID == itemToAdd->ID){
+                Log("failed to add the item: id conflicted");
+                return 1;
+            }
+            ptr = ptr->nextItem;
+        }
+        ptr->nextItem = itemToAdd;
+    }
+    itemNum++;
+
+    Log("item added successfully");
+
+    return 0;
+}
+
+int deleteItem(travelItem *itemToDelete)
+{
+    travelItem *ptr1, *ptr2;
+
+    while(itemHead != NULL && itemHead->ID == itemToDelete->ID){
+        ptr2 = itemHead;
+        itemHead = itemHead->nextItem;
+        free(ptr2);
+    }
+    if(itemHead == NULL)return 0;
+
+    ptr1 = itemHead;
+    ptr2 = itemHead->nextItem;
+    while(ptr2 != NULL){
+        if(ptr2->ID == itemToDelete->ID){
+            ptr1->nextItem = ptr2->nextItem;
+            free(ptr2);
+        }else{
+            ptr1 = ptr2;
+            ptr2 = ptr1->nextItem;
+        }
+    }
+    itemNum--;
+
+    Log("item deleted successfully");
+
     return 0;
 }
--- DevC/TravelManageSystem/fileOperation.h
+++ DevC/TravelManageSystem/fileOperation.h
@@ -2,12 +2,28 @@
 #ifndef _FILE_H_
 #define _FILE_H_
 
+//output debug information in log.txt
+//eg. Log("this is a message");
 void Log(string);
+
+//open the data file in the directory, and load them into an linked list
+//return 1 if the file failed to open
 int openFile();
-int saveFile(travelItem *itemHead,int itemNum);
-int addItem(travelItem);
-int deleteItem(travelItem);
-void creatItem(int ID, Date startDate, Date endDate, double price, 
+
+//save the current data into the data file, but not free the linked list
+//return 1 if the file failed to save
+int saveFile();
+
+//add an item into the linked list
+//return 1 if the id is conflicted, and will not be added
+int addItem(travelItem*);
+
+//delete an item from the linked list, judging by ID
+int deleteItem(travelItem*);
+
+//create an item based on the information given
+//return the point of the item
+travelItem* createItem(int ID, Date startDate, Date endDate, double price, 
                 int numberReserved, int numberTotal, bool isReserved,
                 double rate, char* keyword, char* detail);
 
